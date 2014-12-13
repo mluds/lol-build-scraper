9fad8e89ba791903618ce13fcecba79580cf317e
Michael Ludwig <mluds@ymail.com>
2014-12-13 01:15:41 -0500

    Polished code and removed py2exe content

--- scraper.py
+++ scraper.py
@@ -1,42 +1,30 @@
-# Install using 'pip install requests'
-import requests
+import requests as reqs
 import os
 import json
 
-# Get all champion names
-response = requests.get('http://www.lolflavor.com/data/champion.json')
-response = response.json()
-champions = []
-for champion in response['data']:
-  champions.append(champion)
+def download(dir):
+    domain = 'http://www.lolflavor.com'
+    response = reqs.get(domain + '/data/champion.json').json()
+    champions = [c for c in response['data']]
+    roles = ['lane', 'jungle', 'support', 'aram']
 
-# Each champion has a build for each role
-roles = ['lane', 'jungle', 'support', 'aram']
+    for c in champions:
+      for r in roles:
+        response = reqs.get(domain + '/champions/' + c + '/Recommended/' +
+          c + '_' + r + '_scrape.json').text
 
-# Scrape the builds
-# You may need to change the directory path
-for champion in champions:
-  for role in roles:
-  
-    request = requests.get(
-      'http://www.lolflavor.com/champions/' +
-      champion + '/Recommended/' +
-      champion + '_' + role + '_scrape.json'
-    )
-    
-    try:
-      json.loads(request.text)
-    except ValueError:
-      print('Request text is not valid JSON')
-      continue
-      
-    filename = ('C:/Riot Games/League of Legends/Config/Champions/' +
-      champion + '/Recommended/' + role + '.json')
-      
-    if not os.path.exists(os.path.dirname(filename)):
-      os.makedirs(os.path.dirname(filename))
-      
-    with open(filename, 'w') as f:
-      f.write(request.text)
-      
-    print('Wrote to ' + filename)
+        try:
+          json.loads(response)
+        except ValueError:
+          continue
+
+        file = dir + '/Config/Champions/' + c + '/Recommended/' + r + '.json'
+
+        if not os.path.exists(os.path.dirname(file)):
+          os.makedirs(os.path.dirname(file))
+
+        with open(file, 'w') as f:
+          f.write(response)
+
+if __name__ == '__main__':
+    download('C:/Riot Games/League of Legends')
--- setup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-import py2exe
-
-setup(console=['scraper.py'])

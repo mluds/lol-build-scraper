2c1f0fc46dbc79dd369955c5db4b733837443e6d
Michael Ludwig <mluds@ymail.com>
2015-01-02 16:30:02 -0500

    Added dynamic installation directory

--- scraper.py
+++ scraper.py
@@ -1,30 +1,31 @@
 import requests as reqs
 import os
 import json
+import sys
 
 def download(dir):
-    domain = 'http://www.lolflavor.com'
-    response = reqs.get(domain + '/data/champion.json').json()
-    champions = [c for c in response['data']]
-    roles = ['lane', 'jungle', 'support', 'aram']
+  domain = 'http://www.lolflavor.com'
+  response = reqs.get(domain + '/data/champion.json').json()
+  champions = [c for c in response['data']]
+  roles = ['lane', 'jungle', 'support', 'aram']
 
-    for c in champions:
-      for r in roles:
-        response = reqs.get(domain + '/champions/' + c + '/Recommended/' +
-          c + '_' + r + '_scrape.json').text
+  for c in champions:
+    for r in roles:
+      response = reqs.get(domain + '/champions/' + c + '/Recommended/' +
+        c + '_' + r + '_scrape.json').text
 
-        try:
-          json.loads(response)
-        except ValueError:
-          continue
+      try:
+        json.loads(response)
+      except ValueError:
+        continue
 
-        file = dir + '/Config/Champions/' + c + '/Recommended/' + r + '.json'
+      file = dir + '/Config/Champions/' + c + '/Recommended/' + r + '.json'
 
-        if not os.path.exists(os.path.dirname(file)):
-          os.makedirs(os.path.dirname(file))
+      if not os.path.exists(os.path.dirname(file)):
+        os.makedirs(os.path.dirname(file))
 
-        with open(file, 'w') as f:
-          f.write(response)
+      with open(file, 'w') as f:
+        f.write(response)
 
 if __name__ == '__main__':
-    download('C:/Riot Games/League of Legends')
+  download(sys.argv[1])
